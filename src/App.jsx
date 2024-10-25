5f4ab2a9b9b9197487011f6e548b24404b8eafa3
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-10-25 15:19:10 +0530

    Implemented client application with user authentication, routing, responsive design using Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Navbar from './components/Navbar'
+import Result from './pages/Result'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className='min-h-screen bg-slate-100'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/result' element={<Result/>}/>
+        <Route path='/buy' element={<BuyCredit/>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
